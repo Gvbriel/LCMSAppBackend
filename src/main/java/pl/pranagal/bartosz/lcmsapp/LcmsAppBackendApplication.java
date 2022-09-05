9e9e8858108bbc3ad9f2c3ad9f996a5ed48bad20
BartekPranagal <bpranagal@gmail.com>
2022-09-05 21:26:25 +0200

    Initial commit

---
+++ src/main/java/pl/pranagal/bartosz/lcmsapp/LcmsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.pranagal.bartosz.lcmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcmsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcmsAppBackendApplication.class, args);
+    }
+
+}
