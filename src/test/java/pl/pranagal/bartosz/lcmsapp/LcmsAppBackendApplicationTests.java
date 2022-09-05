b4b5875f235623388bf64472aeb8e5c471736815
BartekPranagal <bpranagal@gmail.com>
2022-09-05 21:26:25 +0200

    Initial commit

---
+++ src/test/java/pl/pranagal/bartosz/lcmsapp/LcmsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pranagal.bartosz.lcmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcmsAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
